6f7eec4ee5f6e81136d9eefe8515a30467218e38
Dominik Bartsch <dominik.bartsch@gmx.net>
2026-03-04 21:44:50 +0100

    Cleanup code

--- sketch.js
+++ sketch.js
@@ -1,26 +1,21 @@
 class Bouncer {
     constructor({
         position = createVector(width/2, height/2),
-        velocity = p5.Vector.random2D(),
+        velocity = p5.Vector.random2D().mult(random(2,5)),
         radius = 15,
-        c = color(random(255), random(255), random(255)),
-        speed = random(1, 5),
+        colorValue = color(random(255), random(255), random(255)),
         mass = 10,
-        maxBursts = 1,
-        maxCollisions = 3,
+        remainingBursts = 1,
+        remainingCollisions = 3,
     } = {}) {
         this.position = position;
         this.velocity = velocity;
         this.radius = radius;
-        this.c = c;
-        this.speed = speed;
+        this.colorValue = colorValue;
         this.mass = mass;
 
-        this.burstCounter = 0;
-        this.maxBursts = maxBursts;
-        this.collisionCounter = 0;
-        this.maxCollisions = maxCollisions;
-        this.velocity.mult(this.speed);
+        this.remainingBursts = remainingBursts;
+        this.remainingCollisions = remainingCollisions;
     }
 
     update() {
@@ -32,25 +27,25 @@ class Bouncer {
         if (this.position.x < 0 + this.radius) {
             this.position.x = this.radius;
             this.velocity.x = -this.velocity.x;
-            this.collisionCounter += 1;
+            this.remainingCollisions -= 1;
         }
         // right wall
         if (this.position.x > width - this.radius) {
             this.position.x = width - this.radius;
             this.velocity.x = -this.velocity.x;
-            this.collisionCounter += 1;
+            this.remainingCollisions -= 1;
         }
         // top wall
         if (this.position.y < 0 + this.radius) {
             this.position.y = this.radius;
             this.velocity.y = -this.velocity.y;
-            this.collisionCounter += 1;
+            this.remainingCollisions -= 1;
         }
         // bottom wall
         if (this.position.y > height - this.radius) {
             this.position.y = height - this.radius;
             this.velocity.y = -this.velocity.y;
-            this.collisionCounter += 1;
+            this.remainingCollisions -= 1;
         }
     }
 
@@ -93,36 +88,36 @@ class Bouncer {
         this.position.add(p5.Vector.mult(n, correction * m2));
         other.position.sub(p5.Vector.mult(n, correction * m1));
 
-        this.collisionCounter++;
-        other.collisionCounter++;
+        this.remainingCollisions -= 1;
+        other.remainingCollisions -= 1;
     }
 
     draw() {
-        fill(this.c);
+        fill(this.colorValue);
         noStroke();
         circle(this.position.x, this.position.y, this.radius * 2);
     }
 
-    isDead() {
-        return this.collisionCounter >= this.maxCollisions;
+    isAlive() {
+        return this.remainingCollisions > 0 ;
     }
 
     // burst into a bunch of smaller bouncers
     burst() {
         let children = [];
-        if (this.burstCounter < this.maxBursts) {
+        if (this.remainingBursts > 0) {
             let parts = 5
             for (let i = 0; i < parts; ++i) {
                 children.push(new Bouncer({
-                    position: p5.Vector.add(this.position, p5.Vector.random2D()),
+                    // add small offset so children don't perfectly overlap
+                    position: p5.Vector.add(this.position, p5.Vector.random2D().mult(0.1)),
                     radius: this.radius / Math.sqrt(parts),
-                    c: this.c,
+                    colorValue: this.colorValue,
                     mass: this.mass / parts,
-                    maxBursts: this.maxBursts - 1,
+                    remainingBursts: this.remainingBursts - 1,
                 }))
             }
         }
-        this.burstCounter += 1;
         return children;
     }
 }
@@ -136,41 +131,47 @@ function setup() {
 function draw() {
     background('white');
 
+    // show counter of current number of bouncers
     fill(0);
     noStroke();
     textSize(16);
     textAlign(LEFT, TOP);
     text("Bouncers: " + bouncers.length, 10, 10);
 
+    // basic update step and collision with walls
     for (let b of bouncers) {
         b.update();
         b.checkBounds();
     }
 
+    // collisions between bouncers
     for (let i = 0; i < bouncers.length; i++) {
         for (let j = i + 1; j < bouncers.length; j++) {
             bouncers[i].checkCollision(bouncers[j]);
         }
     }
 
+    // burst bouncers before they disappear after they max out on collisions
     let bouncer_children = [];
-
     for (let b of bouncers) {
-        if (b.isDead()) {
+        if (!b.isAlive()) {
             bouncer_children.push(...b.burst());
         }
     }
-    
-    bouncers = bouncers.filter(b => !b.isDead());
+
+    bouncers = bouncers.filter(b => b.isAlive());
     bouncers.push(...bouncer_children);
 
+    // draw current bouncers
     for (let b of bouncers) {
         b.draw();
     }
 }
 
+// spawn a new bouncer where the mouse clicks
 function mousePressed() {
     if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
-        bouncers.push(new Bouncer({position: createVector(mouseX, mouseY)}))
+        for (let i = 0; i < 100; i++)
+            bouncers.push(new Bouncer({position: createVector(mouseX, mouseY)}))
     }
 }
